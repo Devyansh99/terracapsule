1b587f63dfc092c3195feb74b9a6af78a47d49e6
Devyansh99 <devyanshag24@gmail.com>
2025-08-30 23:49:12 +0530

    Removed more unwanted files 3

--- fix-logo.ps1
+++
@@ -1,9 +0,0 @@
-$content = Get-Content 'src\app\page.tsx'
-
-# Replace line 912 (index 911)
-$content[911] = '                    <SimpleAnimatedLogo />'
-
-# Replace line 1213 (index 1212) 
-$content[1212] = '                    <SimpleAnimatedLogo />'
-
-$content | Set-Content 'src\app\page.tsx'
